ebd2dd2ecbf7e37a9342530f25ea51b61dc99cef
Daniel Llanos Muñoz <daniel.llanos@worldline.com>
2018-08-20 14:14:12 +0200

    Added prevent monkey clicks extensions and root adapter

---
+++ app/src/main/java/com/worldline/t21kotlinbaseproject/extension/PreventMonkeyClicks.kt
@@ -0,0 +1,20 @@
+package com.worldline.t21kotlinbaseproject.extension
+
+import android.os.SystemClock
+import android.view.View
+
+/**
+ * PreventMonkeyClicks.
+ */
+fun View.clickWithDebounce(debounceTime: Long = 600L, action: () -> Unit) {
+    this.setOnClickListener(object : View.OnClickListener {
+        private var lastClickTime: Long = 0
+
+        override fun onClick(v: View) {
+            if (SystemClock.elapsedRealtime() - lastClickTime < debounceTime) return
+            else action.invoke()
+
+            lastClickTime = SystemClock.elapsedRealtime()
+        }
+    })
+}
