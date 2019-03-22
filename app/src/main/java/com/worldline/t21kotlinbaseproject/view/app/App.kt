1fd338a6362cfc1ce75be0c5e2d8003a2059195c
Jacobo Conejo Lima <jacobo644@gmail.com>
2019-03-22 14:05:16 +0100

    Fixing update to AndroidX
    Update all dependencies and classes to AndroidX and upgrade Gradle and Kotlin plugin versions

--- app/src/main/java/com/worldline/t21kotlinbaseproject/view/app/App.kt
+++ app/src/main/java/com/worldline/t21kotlinbaseproject/view/app/App.kt
@@ -1,13 +1,11 @@
 package com.worldline.t21kotlinbaseproject.view.app
 
 import android.app.Application
-import org.kodein.di.Kodein
-import org.kodein.di.KodeinAware
-import org.kodein.di.lazy
 import com.worldline.t21kotlinbaseproject.di.appModule
 import com.worldline.t21kotlinbaseproject.di.dataModule
 import com.worldline.t21kotlinbaseproject.di.domainModule
-import org.kodein.di.android.x.androidXModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
 
 /**
  * App.
@@ -18,7 +16,6 @@ class App : Application(), KodeinAware {
         import(appModule(this@App))
         import(domainModule)
         import(dataModule)
-        import(androidXModule(this@App))
     }
 
 }
